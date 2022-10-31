4d9607b407d40543e37d7eddf81cb8c3ac55d0ff
Afrohead01 <subhamdeb409@gmail.com>
2022-11-01 00:24:15 +0530

    User in webpage(multiple)

--- 1.js
+++ 1.js
@@ -1,28 +1,9 @@
-function Student(firstName, lastName, rollNo, sex, age){
-    this.firstName = firstName
-    this.lastName =lastName
-    this.rollNo = rollNo
-    this.sex = sex
-    this.age = age
-    this.getFullName = function(){
-        return `${this.firstName} ${this.lastName}`
-    }
+const obj = {
+    key1: 'subham',
+    key2: 'Didhiti',
+    key3: 'Arkadyuti'
 }
-Student.prototype.findEligibleOrNot = function findEligibleOrNot(minAge){
-    if (this.age>minAge){
-        console.log(this.firstName + ' is eligible');
-    }else{
-        console.log(this.firstName + ' is not eligible');
-    }
-};
-const s1 = new Student("subham", "Deb", "01", "male", "9")
-const s2 = new Student("sashi", "sharde", "02", "male", "25")
-if (s1.age>s2.age){
-    console.log(s1)
-}
-else{
-    console.log(s2)
-}
-console.log(s1.getFullName())
-s1.findEligibleOrNot(18)
-s2.findEligibleOrNot(10)
+const arrKey = Object.keys(obj)
+arrKey.forEach(key =>{
+    console.log(obj[key])
+})
